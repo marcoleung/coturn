ea8b533d889a721e96479ae9cd0cb7118ee81051
Marco Leung <marco@oka.hk>
2017-04-13 00:45:18 +0800

    add docker file and deploy script

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# coturn
-docker
+Coturn for Docker
+=================
+
+A Docker container with the Coturn STUN and TURN server (https://code.google.com/p/coturn/)
+
+Pull this image with `docker pull dreamnetwork/coturn`.
+
+You can run a container from this image like this:
+
+```
+docker run -d --name=turnserver --restart="on-failure:10" --net=host -p 3478:3478 -p 3478:3478/udp dreamnetwork/coturn
+```
+
+This will use icanhazip (http://major.io/icanhazip-com-faq/) to determine your container's public IP address. If you don't wish to use icanhazip, or you wish to use an external IP address that doesn't match what icanhazip would see, you can specify it in the environment:
+
+```
+docker run -d -e EXTERNAL_IP=1.2.3.4 --name=turnserver --restart="on-failure:10" --net=host -p 3478:3478 -p 3478:3478/udp dreamnetwork/coturn
+```
+
+Environment Parameters
+-----------------
+* `SKIP_AUTO_IP` -- binds to any address, useful for IPv4 and IPv6 dual-stack when also running with --net=host
+* `EXTERNAL_IP` -- optional manually-specified external IP address
+* `PORT` -- listening port for STUN and TURN
+* `LISTEN_ON_PUBLIC_IP` -- bind to the external IP
+* `USE_IPV4` -- forces IPv4 when determining the external IP
+* `MIN_PORT` -- Lower bound of the UDP port range for relay endpoints allocation. Default value is 49152, according to RFC 5766.
+* `MAX_PORT` -- Upper bound of the UDP port range for relay endpoints allocation. Default value is 65534.
+* `MONGO_HOST` -- User database connection host string for MongoDB
+* `PROD` -- Default verbose mode. PROD will disable verbose mode 
---
+++ turnserver.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+if [ -z $SKIP_AUTO_IP ] && [ -z $EXTERNAL_IP ]
+then
+    if [ ! -z USE_IPV4 ]
+    then
+        EXTERNAL_IP=`curl -4 icanhazip.com 2> /dev/null`
+    else
+        EXTERNAL_IP=`curl icanhazip.com 2> /dev/null`
+    fi
+fi
+
+if [ -z $PORT ]
+then
+    PORT=3478
+fi
+
+if [ ! -e /tmp/turnserver.configured ]
+then
+    if [ -z $SKIP_AUTO_IP ]
+    then
+        echo external-ip=$EXTERNAL_IP > /etc/turnserver.conf
+    fi
+    echo listening-port=$PORT >> /etc/turnserver.conf
+
+    if [ ! -z $LISTEN_ON_PUBLIC_IP ]
+    then
+        echo listening-ip=$EXTERNAL_IP >> /etc/turnserver.conf
+    fi
+    
+    #extra
+    if [ -z $MIN_PORT ]
+    then
+        echo min-port=$MIN_PORT >> /etc/turnserver.conf
+    else
+        echo min-port=49152 >> /etc/turnserver.conf
+    fi
+    
+    if [ -z $MAX_PORT ]
+    then
+        echo man-port=$MAX_PORT >> /etc/turnserver.conf
+    else
+        echo man-port=65534 >> /etc/turnserver.conf
+    fi    
+    
+    if [ ! -z $PROD ]
+    then
+        echo verbose >> /etc/turnserver.conf
+    fi
+    
+    if [ -z $MONGO_HOST ]
+    then
+        echo mongo-userdb=\"mongodb://$MONGO_HOST:27017/coturn\" >> /etc/turnserver.conf
+    fi
+    
+    echo relay-ip=$EXTERNAL_IP >> /etc/turnserver.conf
+    
+    echo relay-threads=5 >> /etc/turnserver.conf
+    echo lt-cred-mech >> /etc/turnserver.conf
+    echo fingerprint >> /etc/turnserver.conf
+    echo realm=pano >> /etc/turnserver.conf
+    echo max-bps=100000 >> /etc/turnserver.conf
+    echo log-file=stdout >> /etc/turnserver.conf
+    echo secure-stun >> /etc/turnserver.conf
+
+    touch /tmp/turnserver.configured
+fi
+
+exec /usr/bin/turnserver >>/var/log/turnserver.log 2>&1
